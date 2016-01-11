14f57018368fd15650f84fa6941ff07e4a73d53c
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-11 18:23:19 -0500

    playing with buttons

--- events/events.js
+++ events/events.js
@@ -1,5 +1,9 @@
 (function() {
+    var link = $('link');
     $('button').click(function(){
-        console.log('button was clicked');
+        var $this = $(this);
+        var stylesheet = $this.data('file');
+        link.attr('href', stylesheet + '.css');
+        $this.attr('disabled' , 'disabled');
     });
 })();
